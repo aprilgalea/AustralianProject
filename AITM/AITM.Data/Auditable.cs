9debd074c338c1f78e0a63e24c65fa7852547eff
aprilgalea <agalea@gladvisor.com>
2014-09-09 12:39:56 -0400

    * Add data project

---
+++ AITM/AITM.Data/Auditable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AITM.Data
+{
+    public class Auditable
+    {
+        public virtual DateTime CreatedDate { get; set; }
+        public virtual string CreatedBy { get; set; }
+        public virtual DateTime LastModifiedDate { get; set; }
+        public virtual string LastModifiedBy { get; set; }
+        public virtual bool IsDeleted { get; set; }
+
+    }
+}
